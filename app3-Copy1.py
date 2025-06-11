d1164ab7abcb96bc96730f73faa315dad7ab172b
Ezibliss <118067640+Ezibliss@users.noreply.github.com>
2025-06-11 22:55:34 +0100

    app3-Copy1.py

--- app3-Copy1.py
+++ app3-Copy1.py
@@ -126,7 +126,7 @@ st.title("🎓 Your Study Companion")
 
 col1, col2 = st.columns([1, 2])
 with col1:
-    st.image("https://yourlogo.url/logo.png", width=150)
+    st.image("Studylogo.png", width=100)
 with col2:
     st.title("Your Study Companion")
 
